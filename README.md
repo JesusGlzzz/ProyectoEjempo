e0523606404605fce465b4c06ee59395b59a4651
jesus gonzalez <jesus_rmz1@hotmail.com>
2022-03-17 15:32:20 -0600

    dev1: documentacion

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Mi primer repositorio en github C#
 Autor: Jesus Gerardo Gonzalez Ramirez
 
 Mas lineas
+
+# Documentacion 1 (dev1)
--- main.cs
+++ main.cs
@@ -7,6 +7,11 @@ namespace PrimeraApp
         static void Main(String [] args)
         {
             Console.Writeline("Hola mundo");
+            // git clone <clone>, clonar un repositorio en mi computadora para manejar el proyecto de manera local
+            // git fetch, despues de clonar el repositorio, descarga todos los archivos
+            // git pull origin main, movia al ulitmo commit
+            // git commit -am "mensaje", guarda de manera local los cambios 
+            // git push origin main, todos los commits los empuja en github 
         }
     }
 }
